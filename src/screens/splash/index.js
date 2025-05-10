9e8887cdad97215a721af8042d374ce9ffab24ec
zainulabideen-dev <zainulabideendev@gmail.com>
2025-05-10 19:18:24 +0500

    enviroment variables

--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,10 +1,12 @@
 import {View, Text, StatusBar, Image} from 'react-native';
 import React, {useEffect} from 'react';
+import {GOOGLE_MAPS_API_KEY} from '@env';
 
 export default function SplashScreen({navigation}) {
   useEffect(() => {
     setTimeout(() => {
-      navigation.replace('LoginScreen');
+      console.log(GOOGLE_MAPS_API_KEY, 'GOOGLE_MAPS_API_KEY');
+      // navigation.replace('LoginScreen');
     }, 3000);
   }, []);
 
