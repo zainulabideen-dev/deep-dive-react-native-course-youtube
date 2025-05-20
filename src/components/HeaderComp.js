6c52f45ace1934f94debc498ad3bd339d8bfc705
zainulabideen-dev <zainulabideendev@gmail.com>
2025-05-20 11:35:39 +0500

    asyn storage and vector icons

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -1,5 +1,6 @@
 import {View, Text, Platform} from 'react-native';
 import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function HeaderComp({name}) {
   return (
@@ -7,13 +8,21 @@ export default function HeaderComp({name}) {
       style={{
         padding: 15,
         backgroundColor: Platform.OS === 'android' ? '#3498db' : 'white',
+        flexDirection: 'row',
+        alignItems: 'center',
       }}>
+      <AntDesign
+        name={Platform.OS === 'android' ? 'arrowleft' : 'left'}
+        size={20}
+        color="white"
+      />
       <Text
         style={{
           fontFamily: 'Poppins-Regular',
           fontSize: Platform.OS === 'ios' ? 25 : 20,
           color: Platform.OS === 'android' ? 'white' : 'black',
           includeFontPadding: false,
+          marginLeft: 15,
         }}>
         {name}
       </Text>
--- src/config/helper.js
+++ src/config/helper.js
@@ -1,4 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export const isValidEmail = email => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
+};
+
+export const storeInToAsynStorage = async (Key, value) => {
+  try {
+    await AsyncStorage.setItem(Key, value);
+  } catch (error) {
+    console.log('=> Error storeInToAsynStorage', error);
+  }
+};
+
+export const getDataFromAsyncStorage = async key => {
+  try {
+    const data = await AsyncStorage.getItem(key);
+    return data;
+  } catch (error) {
+    console.log('=> Error getDataFromAsyncStorage', error);
+  }
+};
+
+export const deleteDataFromAsyncStorage = async key => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (error) {
+    console.log('=> Error deleteDataFromAsyncStorage', error);
+  }
 };
